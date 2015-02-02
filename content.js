1435272e30a9ee96a3d0222cc017795830824961
squit <michael.squitieri@gmail.com>
2015-02-02 01:06:17 -0500

    - add in readability scores - select message based on score - allow reading scores to be shown on items that were loaded through ajax

--- content.js
+++ content.js
@@ -1,19 +1,50 @@
+var CONTAINER_CLASS = 'rate-my-professors-extension';
+
 function isValid(comment) {
   if ($.trim(comment) == 'No Comments') return false;
   return true;
 }
 
-$('.comments p').each(function(i, comment) {
-  var text = $(comment).html(),
-      $div, $header, message;
+function injectReadingScores() {
+  $('.comments p').each(function(i, comment) {
+    var text = $(comment).html();
 
-  if (isValid(text)) {
-    $div = $("<div></div>").addClass('rate-my-professors-extension');
-    $header = $("<h5></h5>");
+    if (isValid(text)) {
+      var $div = $("<div></div>").addClass(CONTAINER_CLASS),
+          $header = $("<h5></h5>"),
+          message, score;
 
-    message = 'This user received a reading score of <strong>123.21</strong> can be <strong class="green">trusted</strong>.';
-    $header.html(message);
-    $div.append($header);
-    $(comment).parent().prepend($div);
-  }
+      score = readability.flesch_kincaid_grade_level(text).toFixed(2);
+
+      message = 'This user received a reading score of <strong>'+score+'</strong> and ';
+      if (score < 4.00) {
+        message += 'should be <strong class="red">ignored</strong>.';
+        $(comment).parents("tr").css('opacity', '0.7');
+      } else if (score < 6.00) {
+        message += 'should be treated with <strong class="orange">caution</strong>.';
+      } else {
+        message += 'can be <strong class="green">trusted</strong>.';
+      }
+      $header.html(message);
+      $div.append($header);
+      $(comment).parent().prepend($div);
+    }
+  });
+}
+
+// Removes added Reading Scores div containers
+// from the page.
+function clearReadingScores() {
+  $('.'+CONTAINER_CLASS).remove();
+}
+
+// Make sure to add the reading scores to new items loaded
+// when user clicks the Load More button.
+$('#loadMore').on('click', function() {
+  setTimeout(function() {
+    clearReadingScores();
+    injectReadingScores();
+  }, 1000);
 });
+
+injectReadingScores();
